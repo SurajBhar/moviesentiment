3c33c243d0cdd6f8d6a9f5a987ea370f2c466a60
SurajBhar <suraj.unisiegen@gmail.com>
2025-07-30 14:06:18 +0200

    Project Structure Created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='End to End Movie Sentiment Prediction App using IMDB Dataset, AWS and Github Actions',
+    author='Suraj Bhardwaj',
+    license='MIT',
+)
